71d12f28a6c4b9843a6971ffc04dbeced495e2cf
Liam <78243032+LiamV32@users.noreply.github.com>
2021-05-12 10:12:42 -0400

    Update validate.js

--- js/validate.js
+++ js/validate.js
@@ -1,5 +1,5 @@
 function validate(event) {
-  // TODO - write custom validation logic to validate the longitude and latitude
+  // custom validation logic to validate the longitude and latitude
   // values. The latitude value must be a number between -90 and 90; the
   // longitude value must be a number between -180 and 180. If either/both are
   // invalid, show the appropriate error message in the form, and stop the 
